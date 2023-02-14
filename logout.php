364f9cbb097b6a94762cac1a8260a3c6dd9c2370
Zard <109944819+FajaarIlhaam@users.noreply.github.com>
2023-02-14 11:46:58 +0700

    update logout

--- logout.php
+++ logout.php
@@ -3,7 +3,8 @@
 session_start();
 
 session_destroy();
-header("location:index.php?pesan=logout");
-
-
 ?>
+<script>
+    sessionStorage.setItem('pesan', 'logout')
+    window.location.href = "http://localhost:8080/swalayanproject"
+</script>
